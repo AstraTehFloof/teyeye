0afe34300cad848bde241a058ebf70f81c6524d3
AstraTehFloof <69868947+AstraTehFloof@users.noreply.github.com>
2022-02-03 20:08:13 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
+local games_metatable = getrawmetatable(game)
+local index = game_metatable.__index
+setreadonly(games_metatable,false)
+games_metatable.__index = function(table,ind)
+    if ind == 'Position' and table.Parent == game.Players.LocalPlayer.Character then 
+        return Vector3.new(167, -40, 2512)
+    end
+    return index(table,ind)
+end
+
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
