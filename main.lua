f90cfcafd62bde1d7a85a3cc19a15cb043f88892
AstraTehFloof <69868947+AstraTehFloof@users.noreply.github.com>
2022-02-03 19:33:44 -0600

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,28 @@
+while wait() do
+--Hitpart >
+local hitpart
+
+for i,v in pairs(game.Workspace:WaitForChild("enemies"):GetDescendants()) do
+if v.Name == "Head" then
+    v = v
+    hitpart = v
+end
+end
+
+
+
+
+local args = {
+    [1] = {
+        ["Normal"] = Vector3.new(hitpart.CFrame.Position),
+        ["Direction"] = Vector3.new(hitpart.CFrame.Position),
+        ["Name"] = _G.gunname,
+        ["Hit"] = hitpart,
+        ["Origin"] = Vector3.new(hitpart.CFrame.Position),
+        ["Pos"] = Vector3.new(hitpart.CFrame.Position),
+    }
+}
+
+game:GetService("ReplicatedStorage").Gun:FireServer(unpack(args))
+hitpart = ""
+end
